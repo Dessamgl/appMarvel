2c3a1b252c5db77220514d10d5269e9c998d48e0
Andressa Gaspar <andressa.luiz@softdesign.com.br>
2020-02-04 17:20:01 -0300

    Personalização header e ajustes

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { StatusBar } from 'react-native';
+import listHeroes, {homeConfig} from './src/pages/listHeroes'
+import infoHeroes from './src/pages/infoHeroes';
 
-import Routes from './src/routes';
+const Root = createStackNavigator({
+  listHeroes: {
+    screen: listHeroes,
+    navigationOptions: homeConfig
+  },
+  infoHeroes: {
+    screen: infoHeroes,
+    navigationOptions: homeConfig
+  }
+});
+
+const Navigation = createAppContainer(Root);
 
 export default function App() {
   return ( 
   <>
     <StatusBar barStyle='light-content'  backgroundColor='#B50F16' />
-    <Routes />
+    <Navigation />
   </>
   );
 }
--- src/pages/listHeroes.js
+++ src/pages/listHeroes.js
@@ -1,18 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { TouchableOpacity, View, FlatList, Text, Image, TextInput } from 'react-native'
+import { TouchableOpacity, View, FlatList, Text, Image, Action } from 'react-native'
+import { Header} from "react-native-elements";
 import * as Font from 'expo-font';
 import { MaterialIcons } from '@expo/vector-icons';
 import md5 from 'js-md5'
+import { TextInput } from 'react-native-gesture-handler';
 
 const PUBLIC_KEY = '2f2331c74c430b98e884e694b0c722cc'
 const PRIVATE_KEY = 'a951417c6542664dda96ea1f031cd75a50b3c3c7'
 
-
+export function homeConfig({navigation}) {
+    return {
+        header: null
+    }
+}
 function ListHeroes({ navigation }) {
     Font.loadAsync({
         'barlow-black': require('../../assets/fonts/Barlow-Black.ttf'),
+        'barlow-condensed': require('../../assets/fonts/BarlowCondensed-Black.ttf')
       })
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [active, setActive] = useState(false);
     
     state = {
         data: []
@@ -45,19 +53,84 @@ function ListHeroes({ navigation }) {
                     style={{height: 50, width: 50, borderRadius: 25}} 
                     source={{uri: `${item.thumbnail.path}.${item.thumbnail.extension}` }} 
                 />
-                <Text style={{marginLeft: 10, color: "#ffffff", fontFamily: 'barlow-black'}}>{item.name}</Text>
-                <MaterialIcons name="keyboard-arrow-right" size={40} color="#F0141E" style={{
+                <Text style={{marginLeft: 10, color: "#ffffff", fontFamily: 'barlow-condensed', fontStyle: "normal", fontWeight: "normal", fontSize: 16, lineHeight: 19}}>{item.name}</Text>
+                <MaterialIcons name="keyboard-arrow-right" size={35} color="#F0141E" style={{
                     position: "absolute",
                     height: 50,
-                    left: 333,
+                    left: 340,
                     top: 20,
                     mixBlendMode: "normal"
                 }}></MaterialIcons>
             </TouchableOpacity>
         )
     }
-
+    
+    if(!active){
         return (
+            <>
+            <Header
+                backgroundColor={'#B50F16'}
+                leftComponent={{
+                    style: {color: '#fff', fontSize: 15, fontFamily: 'barlow-black' },
+                    text: "Characters"
+                }}
+                rightComponent={
+                    <TouchableOpacity>
+                        <MaterialIcons name="search" size={25} color="#ffffff" style={{
+                            height: 20
+                        }} onPress={()=> {
+                            navigation.navigate('infoHeroes', {hero: item})
+                        }}>
+                        </MaterialIcons>
+                    </TouchableOpacity> 
+                }
+            />
+            </>
+        )
+    } else {
+        return (
+            <>
+            <Header
+            backgroundColor={'#B50F16'}
+                rightComponent={ 
+                    <TouchableOpacity
+                    onPress={()=> {
+                        setActive(false);
+                    }}>
+                        <MaterialIcons name="close" size={25} color="#ffffff" style={{
+                            height: 20
+                        }} >
+                        </MaterialIcons>
+                    </TouchableOpacity> 
+                }
+                centerComponent={
+                    <View>
+                        <TextInput
+                            placeholder="Name of character"
+                            placeholderTextColor="#999"
+                            autoCapitalize="words"
+                            autoCorrect={false}
+                        >
+                        </TextInput>
+                    </View>
+                }
+                leftComponent={ 
+                    <TouchableOpacity>
+                        <MaterialIcons name="arrowleft" size={25} color="#ffffff" style={{
+                            height: 20
+                        }} onPress={()=> {
+                            setActive(true);
+                            navigation.goBack()
+                        }}>
+                        </MaterialIcons>
+                    </TouchableOpacity> 
+                }
+            />
+            </>
+        )
+      }
+            
+            return (
             <FlatList style={{ backgroundColor: '#303030'}}
             data={data}
             renderItem={renderItem}
@@ -66,7 +139,7 @@ function ListHeroes({ navigation }) {
                 <View style={{height:1, backgroundColor: '#000000'}} 
                 />}
             />
-        )
+    )
 }
 
 export default ListHeroes;
