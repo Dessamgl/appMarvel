396b48eae3a0d7aa772b6dfbd1f3c8f368bfb2f2
Dessamgl <40497345+Dessamgl@users.noreply.github.com>
2020-02-05 23:46:48 -0300

    expandir texto

--- src/pages/infoHeroes.js
+++ src/pages/infoHeroes.js
@@ -1,25 +1,65 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, ScrollView, Image, Dimensions, Text } from 'react-native';
-
-const SCREEN_WIDTH = Dimensions.get('screen').width
+import { MaterialIcons } from '@expo/vector-icons';
 
 function infoHeroes({ navigation }) {
 
-        const { hero } = navigation.state.params
-        return (
-           <ScrollView persistentScrollbar={true}  style={{backgroundColor: '#303030',}}>
-               <Image 
-                    source={{uri: `${hero.thumbnail.path}.${hero.thumbnail.extension}`}} 
-                    style={styles.image}
-                />
-                <Text style={styles.name}>{hero.name || "Nome do Personagem"}</Text>
-                <Text style={styles.description}>{hero.description || 
-                "Our writer is busy saving the Semantics World, so this character doesn't have a description yet!"}
-                </Text>
-           </ScrollView> 
-        )
+    const { hero } = navigation.state.params
 
-    }
+    const [active, setActive] = useState(false);
+
+    
+    if(active){
+        styles.description = {height: 500, width: 380,
+            textAlign: "center",
+            padding: 40,
+            fontStyle: "normal",
+            fontWeight: "normal",
+            fontSize: 16,
+            lineHeight: 19,
+            color: "#FFFFFF"}
+    } else {
+        styles.description = {height: 150,  width: 380,
+            textAlign: "center",
+            padding: 40,
+            fontStyle: "normal",
+            fontWeight: "normal",
+            fontSize: 16,
+            lineHeight: 19,
+            color: "#FFFFFF"}
+        }
+
+
+    return (
+        <ScrollView persistentScrollbar={true}  style={{backgroundColor: '#303030',}}>
+            <Image 
+                source={{uri: `${hero.thumbnail.path}.${hero.thumbnail.extension}`}} 
+                style={styles.image}
+            />
+            <Text 
+            style={styles.name}>{hero.name || "Nome do Personagem"}
+            </Text>
+
+            <Text 
+            style={styles.description}>{hero.description || 
+            "Our writer is busy saving the Semantics World, so this character doesn't have a description yet!"}
+            </Text>
+            <MaterialIcons 
+                        style={styles.icon}
+                        onPress={()=> {
+                         setActive(true)
+                        }}
+                        name="down" 
+                        size={25} 
+                        color="#ffffff" 
+            >
+
+            </MaterialIcons>
+        </ScrollView> 
+    )
+
+}
+
     const styles = StyleSheet.create({
         image: {
             borderRadius: 80,
@@ -38,15 +78,22 @@ function infoHeroes({ navigation }) {
             lineHeight: 24,
             color: "#FFFFFF", 
         },
-        description: {
-            width: 380,
+        // description: {
+        // //    height:150,
+        //     width: 380,
+        //     textAlign: "center",
+        //     padding: 40,
+        //     fontStyle: "normal",
+        //     fontWeight: "normal",
+        //     fontSize: 16,
+        //     lineHeight: 19,
+        //     color: "#FFFFFF"
+        // }
+        icon:{
+            color:"#F0141E", 
             textAlign: "center",
-            padding: 40,
-            fontStyle: "normal",
-            fontWeight: "normal",
-            fontSize: 16,
-            lineHeight: 19,
-            color: "#FFFFFF"
+            // height: 20
+            width: 380
         }
     })
 
--- src/pages/listHeroes.js
+++ src/pages/listHeroes.js
@@ -4,7 +4,6 @@ import { Header} from "react-native-elements";
 import * as Font from 'expo-font';
 import { MaterialIcons } from '@expo/vector-icons';
 import md5 from 'js-md5'
-import { TextInput } from 'react-native-gesture-handler';
 
 const PUBLIC_KEY = '2f2331c74c430b98e884e694b0c722cc'
 const PRIVATE_KEY = 'a951417c6542664dda96ea1f031cd75a50b3c3c7'
@@ -14,11 +13,13 @@ export function homeConfig({navigation}) {
         header: null
     }
 }
+
 function ListHeroes({ navigation }) {
     Font.loadAsync({
         'barlow-black': require('../../assets/fonts/Barlow-Black.ttf'),
         'barlow-condensed': require('../../assets/fonts/BarlowCondensed-Black.ttf')
       })
+
     const [data, setData] = useState([]);
     const [active, setActive] = useState(false);
     
@@ -53,19 +54,36 @@ function ListHeroes({ navigation }) {
                     style={{height: 50, width: 50, borderRadius: 25}} 
                     source={{uri: `${item.thumbnail.path}.${item.thumbnail.extension}` }} 
                 />
-                <Text style={{marginLeft: 10, color: "#ffffff", fontFamily: 'barlow-condensed', fontStyle: "normal", fontWeight: "normal", fontSize: 16, lineHeight: 19}}>{item.name}</Text>
-                <MaterialIcons name="keyboard-arrow-right" size={35} color="#F0141E" style={{
-                    position: "absolute",
-                    height: 50,
-                    left: 340,
-                    top: 20,
-                    mixBlendMode: "normal"
-                }}></MaterialIcons>
+                <Text 
+                    style={{
+                        marginLeft: 10, 
+                        color: "#ffffff", 
+                        fontFamily: 'barlow-condensed', 
+                        fontStyle: "normal", 
+                        fontWeight: "normal", 
+                        fontSize: 16, 
+                        lineHeight: 19
+                    }}>
+                        {item.name}
+                </Text>
+
+                <MaterialIcons 
+                    name="keyboard-arrow-right" 
+                    size={35} 
+                    color="#F0141E" 
+                    style={{
+                        position: "absolute",
+                        height: 50,
+                        left: 340,
+                        top: 20,
+                        mixBlendMode: "normal"
+                    }}>
+
+                </MaterialIcons>
             </TouchableOpacity>
         )
     }
     
-    if(!active){
         return (
             <>
             <Header
@@ -75,71 +93,33 @@ function ListHeroes({ navigation }) {
                     text: "Characters"
                 }}
                 rightComponent={
-                    <TouchableOpacity>
-                        <MaterialIcons name="search" size={25} color="#ffffff" style={{
+                    <TouchableOpacity  
+                    onPress={()=> {}}>
+
+                        <MaterialIcons 
+                        name="search" 
+                        size={25} 
+                        color="#ffffff" 
+                        style={{
                             height: 20
-                        }} onPress={()=> {
-                            navigation.navigate('infoHeroes', {hero: item})
                         }}>
+
                         </MaterialIcons>
                     </TouchableOpacity> 
                 }
             />
-            </>
-        )
-    } else {
-        return (
-            <>
-            <Header
-            backgroundColor={'#B50F16'}
-                rightComponent={ 
-                    <TouchableOpacity
-                    onPress={()=> {
-                        setActive(false);
-                    }}>
-                        <MaterialIcons name="close" size={25} color="#ffffff" style={{
-                            height: 20
-                        }} >
-                        </MaterialIcons>
-                    </TouchableOpacity> 
-                }
-                centerComponent={
-                    <View>
-                        <TextInput
-                            placeholder="Name of character"
-                            placeholderTextColor="#999"
-                            autoCapitalize="words"
-                            autoCorrect={false}
-                        >
-                        </TextInput>
-                    </View>
-                }
-                leftComponent={ 
-                    <TouchableOpacity>
-                        <MaterialIcons name="arrowleft" size={25} color="#ffffff" style={{
-                            height: 20
-                        }} onPress={()=> {
-                            setActive(true);
-                            navigation.goBack()
-                        }}>
-                        </MaterialIcons>
-                    </TouchableOpacity> 
-                }
-            />
-            </>
-        )
-      }
-            
-            return (
-            <FlatList style={{ backgroundColor: '#303030'}}
-            data={data}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.id}
-            ItemSeparatorComponent={()=>
+
+            <FlatList 
+                style={{ backgroundColor: '#303030'}}
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+                ItemSeparatorComponent={()=>
                 <View style={{height:1, backgroundColor: '#000000'}} 
                 />}
             />
-    )
+            </>
+        )
 }
 
 export default ListHeroes;
